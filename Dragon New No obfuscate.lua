08aad924fdc2ad6342f1838c2a1a8e5bbc213473
altaccountv1 <144928943+altaccountv1@users.noreply.github.com>
2024-02-08 06:57:00 +0000

    Update Dragon New No obfuscate.lua

--- Dragon New No obfuscate.lua
+++ Dragon New No obfuscate.lua
@@ -43,7 +43,7 @@ local plre = game.Players.LocalPlayer
 local WV = "nil."
 local DName = game.Players.LocalPlayer.DisplayName
 local UName = game.Players.LocalPlayer.Name
-_G.WV = "nil."
+_G.WV = "nil"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/altaccountv1/-CLASSIFIED-./main/Classified"))();
 
