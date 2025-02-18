dd5a6848a00fbfb02aaab0ce70cf404170d6183d
altaccountv1 <144928943+altaccountv1@users.noreply.github.com>
2025-02-18 07:41:09 +0000

    Update Dragon New No obfuscate.lua

--- Dragon New No obfuscate.lua
+++ Dragon New No obfuscate.lua
@@ -100,7 +100,7 @@ if _G.DragonConfigurations.Experiments == true then
         }
         args[1][4] = newCF
 
-        ME:FireServer(unpack(args))
+        ME:FireServer(unpack(args)) task.wait(0.07)
         createTrail(hrp.Position, target.Position, Color3.new(1, 0, 0))
     end
 
