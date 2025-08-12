7acdaf7578c05e8ba9fa5b7ace1da685d20a44dd
altaccountv1 <144928943+altaccountv1@users.noreply.github.com>
2025-08-12 21:29:18 +0200

    Update Dragon New No obfuscate.lua

--- Dragon New No obfuscate.lua
+++ Dragon New No obfuscate.lua
@@ -20,7 +20,7 @@ local menu = pgui.MenuUI.Menu
 local abil = menu.Abilities.Frame.Frame.Frame
 local abilFolder = game.ReplicatedStorage.Abilities.Brawler
 
-if _G.AuraSyncAddOn == true
+if _G.AuraSyncAddOn == true then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ducksy9340/R2FModding/refs/heads/main/AuraSyncer.lua"))();
 end
 
@@ -1901,4 +1901,5 @@ Notify("Dragon Style Loaded!",nil,Color3.fromRGB(3,161,252),"RobotoMono")
 Forcefield:Destroy()
 startsound.Ended:Wait()
 startsound:Destroy()
+
 
